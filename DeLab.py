c1d6f265571c316d25309248e638da81b89d04c6
Rommel Contreras <14-10242@usb.ve>
2022-07-21 21:19:26 -0400

    Se actualizaron los archivos a las nuevas especificaciónes

--- DeLab.py
+++ DeLab.py
@@ -10,6 +10,7 @@ from pprint import pp
 ########################################## Urls #############################################################
 
 urlo = "https://api.meraki.com/api/v1/organizations"
+urld="https://api.meraki.com/api/v1/organizations/681155/devices"
 
 ########################################## Headers/Payload ###################################################
 payload = None
@@ -21,6 +22,8 @@ headers = {
 ########################################## Default requests ##################################################
 response = requests.request('GET', urlo, headers=headers, data = payload)
 orgData= json.loads(response.text)
+response= requests.request('GET', urld, headers=headers, data = payload)
+orgDev= json.loads(response.text)
 
 ######################################### functions ###########################################################
 
@@ -28,8 +31,80 @@ def org_Data(): #Consulta la data de las organizaciones a las que tenemos
     response = requests.request('GET', urlo, headers=headers, data = payload)
     orgData= json.loads(response.text)
 
+def org_Dev(): #Consulta los dispositivos de DeLab
+    response= requests.request('GET', urld, headers=headers, data = payload)
+    orgDev= json.loads(response.text)
+
+def  oid_DeLab(): #busca el organization Id de DeLab
+    DeLab=[]
+    for i in range(len(orgData)):
+        if(orgData[i]['name']=='DeLab'):    	    
+            DeLab.append(orgData[i])
+        DeLab.append(0)
+    return DeLab[0]['id']
+
 def printallOrg(): #lista todas las organizaciones
     pp(orgData)
-    
+
 def printOrg(i): #imprime una organización en específico
     pp(orgData[i])
+
+def printallOrgDev(): #imprime todos los dispositivos de la organización DeLab
+    pp(orgDev)
+
+def printOrgDev(i):   #imprime un dispositivo en específico de la organización DeLab
+    pp(orgDev[i])
+
+def productType(): #retorna una lista de dicionario con los datos de los dispositivos tipo: Wireless y appliance
+    pt=[]
+    for i in range(len(orgDev)):
+        if(orgDev[i]["productType"]== "appliance"):
+            pt.append(orgDev[i])
+        if(orgDev[i]["productType"]== "wireless"):
+            pt.append(orgDev[i])
+    return pt
+
+def formpt(p):
+    for i in range(len(p)):
+        pcopy = p[i].copy()
+        p[i].clear()
+
+        if pcopy["productType"]=="wireless":
+            p[i]["productType"] = pcopy["productType"]
+            p[i]["model"] = pcopy["model"]
+            p[i]["name"] = pcopy["name"]
+            p[i]["mac"] = pcopy["mac"]
+            p[i]["lanIp"] = pcopy["lanIp"]
+            p[i]["serial"] = pcopy["serial"]
+        else:
+            p[i]["productType"] = pcopy["productType"]
+            p[i]["model"] = pcopy["model"]
+            p[i]["name"] = pcopy["name"]
+            p[i]["mac"] = pcopy["mac"]
+            p[i]["wan1Ip"] = pcopy["wan1Ip"]
+            p[i]["wan2Ip"] = pcopy["wan2Ip"]
+            p[i]["serial"] = pcopy["serial"]
+        
+def jsontocsv(p): #genera un archivo con extensión .csv con el inventario de los dispositivos Wireless y Appliance
+    blank=[]
+    header=p[0].keys()
+    with open('inventory.csv',mode='w') as inventory:
+        inventory_writer= csv.writer(inventory,delimiter=',')
+        inventory_writer.writerow(header)
+        for i in range(len(p)):
+            if(p[0]['productType']==p[i]['productType']):
+                inventory_writer.writerow(p[i].values())
+        i=0
+        while(p[0]['productType']==p[i]['productType']):
+            i=1+i
+        header=p[i].keys()
+        inventory_writer.writerow(blank)
+        inventory_writer.writerow(header)
+        for i in range(len(p)):
+            if(p[0]['productType']!=p[i]['productType']):
+                inventory_writer.writerow(p[i].values())
+
+def printcsvdata(): #imprime en el terminal la data del inventario en formato .json
+    p=productType()
+    formpt(p)
+    pp(p)
--- README.md
+++ README.md
@@ -3,9 +3,32 @@ Tarea de SDN
 
 El programa por defecto hace una consulta de las organizaciones a las que tenemos acceso con la clave el API_KEY entregado y guarda la respuesta en la variable orgData
 
+Se le agregó una nueva feature para tambien consultar los dispositivos asociados a DebLAb
+
+y generar un archivo csv con el inventario de dispositivos wireless y appliance
+
 org_Data(): #Consulta la data de las organizaciones a las que tenemos
 
 def printallOrg(): #lista todas las organizaciones
 
 def printOrg(i): #imprime una organización en específico
+
+oid_DeLab(): #busca el organization Id de DeLab
+
+printallOrg(): #lista todas las organizaciones
+
+printOrg(i): #imprime una organización en específico
+
+printallOrgDev(): #imprime todos los dispositivos de la organización DeLab
+
+printOrgDev(i):   #imprime un dispositivo en específico de la organización DeLab
+
+productType(): #retorna una lista de dicionario con los datos de los dispositivos tipo: Wireless y appliance
+
+formpt(p):
+
+jsontocsv(p): #genera un archivo con extensión .csv con el inventario de los dispositivos Wireless y Appliance
+
+printcsvdata(): #imprime en el terminal la data del inventario en formato .json
+
 # DeLab
